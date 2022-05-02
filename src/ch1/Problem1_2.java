39fb4f221b31af1760f4ba8092539063c96c1132
spencerdavis44 <104582416+spencerdavis44@users.noreply.github.com>
2022-05-01 21:17:03 -0500

    Add files via upload

---
+++ src/ch1/Problem1_2.java
@@ -0,0 +1,13 @@
+package ch1;
+/*
+ * Write a program that displays Welcome to Java five times
+ */
+public class Problem1_2 {
+	public static void main(String[] args ){
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+	}
+}
